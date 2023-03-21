0a86ea751da95c0c9ee3b3a1a22004346be92fe5
Fariha kabir khan <farihakabirkhan@gmail.com>
2023-03-21 17:32:18 +0600

    Change Statup

--- School_Management_System/Startup.cs
+++ School_Management_System/Startup.cs
@@ -61,9 +61,9 @@ namespace School_Management_System
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
 
-                //routes.MapRoute(
-                //    name: "areas",
-                //    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 
             });
